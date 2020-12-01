5f975c79b48196a1a5b206db9552985bed60d12e
Francisco Orlandini <francisco.orlandini@gmail.com>
2020-12-01 09:12:03 +0100

    fixed indent on conky-spotify/cover.sh

--- scripts/cover.sh
+++ scripts/cover.sh
@@ -6,24 +6,22 @@ cover=
 imgurl=
 
 if [ "$id_new" != "$id_current" ]; then
-
+    cover=`ls ~/.config/conky/conky-spotify/covers | grep $id_new`
+    
+    if [ "$cover" == "" ]; then
+	imgurl=`~/.config/conky/conky-spotify/scripts/imgurl.sh $id_new`
+	wget -q -O ~/.config/conky/conky-spotify/covers/$id_new.jpg $imgurl &> /dev/null
+	# clean up covers folder, keeping only the latest X amount, in below example it is 10
+	rm -f `ls -t ~/.config/conky/conky-spotify/covers/* | awk 'NR>10'`
+	rm -f wget-log #wget-logs are accumulated otherwise
 	cover=`ls ~/.config/conky/conky-spotify/covers | grep $id_new`
-
-	if [ "$cover" == "" ]; then
-
-	    imgurl=`~/.config/conky/conky-spotify/scripts/imgurl.sh $id_new`
-	    wget -q -O ~/.config/conky/conky-spotify/covers/$id_new.jpg $imgurl &> /dev/null
-		# clean up covers folder, keeping only the latest X amount, in below example it is 10
-	    rm -f `ls -t ~/.config/conky/conky-spotify/covers/* | awk 'NR>10'`
-	    rm -f wget-log #wget-logs are accumulated otherwise
-	    cover=`ls ~/.config/conky/conky-spotify/covers | grep $id_new`
-	fi
-
-	if [ "$cover" != "" ]; then
-		cp ~/.config/conky/conky-spotify/covers/$cover ~/.config/conky/conky-spotify/current/current.jpg
-	else
-		cp ~/.config/conky/conky-spotify/empty.jpg ~/.config/conky/conky-spotify/current/current.jpg
-	fi
-
-	echo $id_new > ~/.config/conky/conky-spotify/current/current.txt
+    fi
+    
+    if [ "$cover" != "" ]; then
+	cp ~/.config/conky/conky-spotify/covers/$cover ~/.config/conky/conky-spotify/current/current.jpg
+    else
+	cp ~/.config/conky/conky-spotify/empty.jpg ~/.config/conky/conky-spotify/current/current.jpg
+    fi
+    
+    echo $id_new > ~/.config/conky/conky-spotify/current/current.txt
 fi
